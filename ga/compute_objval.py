f5ca189f0b64ead1310b18ffa114259857b741e4
Hiroaki Imoto <xyz011aax@gmail.com>
2019-01-08 16:43:40 +0900

    Updated Files:

--- ga/compute_objval.py
+++ ga/compute_objval.py
@@ -1,12 +1,12 @@
 from scipy.spatial.distance import cosine
 
-def compute_objval_abs(simData,exData):#Residual Sum of Squares
-    return np.dot((simData-exData),(simData-exData))
+def compute_objval_abs(simData,expData):#Residual Sum of Squares
+    return np.dot((simData-expData),(simData-expData))
 
-def compute_objval_cs(simData,exData):#Cosine similarity
-    return cosine(simData,exData)
+def compute_objval_cs(simData,expData):#Cosine similarity
+    return cosine(simData,expData)
 
-def compute_objval_cs2(simData_c1,simData_c2,exData_c1,exData_c2):#Cosine similarity
+def compute_objval_cs2(simData_c1,simData_c2,expData_c1,expData_c2):#Cosine similarity
     simData = np.append(simData_c1,simData_c2)
-    exData = np.append(exData_c1,exData_c2)
-    return cosine(simData,exData)
+    expData = np.append(expData_c1,expData_c2)
+    return cosine(simData,expData)
