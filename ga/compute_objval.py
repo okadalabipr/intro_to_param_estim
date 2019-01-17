139766c11c4b841f77361b90b642b95fef425cb0
Hiroaki Imoto <xyz011aax@gmail.com>
2019-01-17 18:56:33 +0900

    Removed compute_objval_cs2

--- ga/compute_objval.py
+++ ga/compute_objval.py
@@ -5,8 +5,3 @@ def compute_objval_abs(simData,expData):#Residual Sum of Squares
 
 def compute_objval_cs(simData,expData):#Cosine similarity
     return cosine(simData,expData)
-
-def compute_objval_cs2(simData_c1,simData_c2,expData_c1,expData_c2):#Cosine similarity
-    simData = np.append(simData_c1,simData_c2)
-    expData = np.append(expData_c1,expData_c2)
-    return cosine(simData,expData)
