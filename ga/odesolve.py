ffbb6f2f253d604f856f73a4d8a51d67e6428263
Hiroaki Imoto <himoto@protein.osaka-u.ac.jp>
2019-01-25 09:43:58 +0900

    Rename odesolve arguments

--- ga/odesolve.py
+++ ga/odesolve.py
@@ -1,16 +1,16 @@
 from scipy.integrate import ode
 
-def odesolve(DIFFEQ,Y_ZERO,TSPAN,ARGS):
-    sol = ode(DIFFEQ)
+def odesolve(diffeq,y0,tspan,x):
+    sol = ode(diffeq)
     sol.set_integrator('vode',method='bdf',min_step=1e-8,with_jacobian=True)
-    sol.set_initial_value(Y_ZERO,TSPAN[0])
-    sol.set_f_params(ARGS)
+    sol.set_initial_value(y0,tspan[0])
+    sol.set_f_params(x)
 
-    T = [TSPAN[0]]
-    Y = [Y_ZERO]
+    T = [tspan[0]]
+    Y = [y0]
 
-    while sol.successful() and sol.t < TSPAN[-1]:
-        #sol.integrate(TSPAN[-1],step=True)
+    while sol.successful() and sol.t < tspan[-1]:
+        #sol.integrate(tspan[-1],step=True)
         sol.integrate(sol.t+1.)
         T.append(sol.t)
         Y.append(sol.y)
