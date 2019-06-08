f72abdc2c67f5c6d2b4403df8aa16332fd7659fc
Hiroaki Imoto <himoto@protein.osaka-u.ac.jp>
2019-06-08 16:54:40 +0900

    Update run_sim.py

--- runSim/run_sim.py
+++ runSim/run_sim.py
@@ -12,24 +12,23 @@ if not os.path.isdir('./Fig'):
     os.mkdir('./Fig')
 
 
-def include(src_file):
-    src_dir = '../src/'
-    if '.py' in src_file:
-        with open(src_dir+src_file,'r',encoding='utf-8') as f:
+def include(pyfile):
+    if '.py' in pyfile:
+        with open('../'+pyfile,'r',encoding='utf-8') as f:
             script = f.read()
             exec(script,globals())
     else:
-        files = glob.glob(src_dir+src_file)
+        files = glob.glob('../'+pyfile)
         for file in files:
-            include(file[len(src_dir):])
+            include(file[len('../'):])
 
 
-include('model/f_parameter.py')
-include('model/f_variable.py')
-include('model/differential_equation.py')
-include('model/initial_condition.py')
-include('experimental_data.py')
-include('search_parameter.py')
-include('simulation.py')
-include('plot_func.py')
-include('viz.py')
+include('src/model/f_parameter.py')
+include('src/model/f_variable.py')
+include('src/model/differential_equation.py')
+include('src/model/initial_condition.py')
+include('src/experimental_data.py')
+include('src/search_parameter.py')
+include('src/simulation.py')
+include('src/plot_func.py')
+include('src/viz.py')
