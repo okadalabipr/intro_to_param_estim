603f983c955f05025c957c1232e475ab31676554
himoto <himoto@protein.osaka-u.ac.jp>
2019-09-24 21:16:57 +0900

    Update fitness.py

--- param_estim/fitness.py
+++ param_estim/fitness.py
@@ -5,7 +5,7 @@ from .model.name2idx import parameters as C
 from .model.name2idx import variables as V
 from .model.param_const import f_params
 from .model.initial_condition import initial_values
-from .observable import dynamics,diff_sim_and_exp
+from .observable import *
 from .simulation import Simulation
 from .experimental_data import ExperimentalData
 from .genetic_algorithm.converter import decode_gene2variable
@@ -53,82 +53,18 @@ def objective(individual_gene,search_idx,search_region):
     sim = Simulation()
 
     if sim.simulate(x,y0) is None:
-        error = np.zeros(7)
-
-        # ERK
-        norm_max = np.max(sim.simulations[dynamics['Phosphorylated_ERKc']])
-        error[0] = compute_objval_abs(
-            *diff_sim_and_exp(
-                sim.simulations[dynamics['Phosphorylated_ERKc']],
-                exp.data[dynamics['Phosphorylated_ERKc']],
-                exp_timepoint=exp.t2,num_condition=sim.condition,
-                norm_max_sim=norm_max
+        error = np.zeros(num_observables)
+        for i,target in enumerate(observable_names):
+            exp_t = exp.get_timepoint(i)
+            norm_max = np.max(sim.simulations[dynamics[target]])
+            error[i] = compute_objval_abs(
+                *diff_sim_and_exp(
+                    sim.simulations[dynamics[target]],
+                    exp.data[dynamics[target]],
+                    exp_timepoint=exp_t,num_condition=sim.condition,
+                    norm_max_sim=norm_max
+                )
             )
-        )
-        
-        # RSK
-        norm_max = np.max(sim.simulations[dynamics['Phosphorylated_RSKw']])
-        error[1] = compute_objval_abs(
-            *diff_sim_and_exp(
-                sim.simulations[dynamics['Phosphorylated_RSKw']],
-                exp.data[dynamics['Phosphorylated_RSKw']],
-                exp_timepoint=exp.t2,num_condition=sim.condition,
-                norm_max_sim=norm_max
-            )
-        )
-        # CREB
-        norm_max = np.max(sim.simulations[dynamics['Phosphorylated_CREBw']])
-        error[2] = compute_objval_abs(
-            *diff_sim_and_exp(
-                sim.simulations[dynamics['Phosphorylated_CREBw']],
-                exp.data[dynamics['Phosphorylated_CREBw']],
-                exp_timepoint=exp.t3,num_condition=sim.condition,
-                norm_max_sim=norm_max
-            )
-        )
-        # DUSPmRNA
-        norm_max = np.max(sim.simulations[dynamics['dusp_mRNA']])
-        error[3] = compute_objval_abs(
-            *diff_sim_and_exp(
-                sim.simulations[dynamics['dusp_mRNA']],
-                exp.data[dynamics['dusp_mRNA']],
-                exp_timepoint=exp.t5,num_condition=sim.condition,
-                norm_max_sim=norm_max
-            )
-        )
-        # cFosmRNA
-        norm_max = np.max(sim.simulations[dynamics['cfos_mRNA']])
-        error[4] = compute_objval_abs(
-            *diff_sim_and_exp(
-                sim.simulations[dynamics['cfos_mRNA']],
-                exp.data[dynamics['cfos_mRNA']],
-                exp_timepoint=exp.t4,num_condition=sim.condition,
-                norm_max_sim=norm_max
-            )
-        )
-        # cFosPro
-        norm_max = np.max(sim.simulations[dynamics['cFos_Protein']])
-        error[5] = compute_objval_abs(
-            *diff_sim_and_exp(
-                sim.simulations[dynamics['cFos_Protein']],
-                exp.data[dynamics['cFos_Protein']],
-                exp_timepoint=exp.t5,num_condition=sim.condition,
-                norm_max_sim=norm_max
-            )
-        )
-        # PcFos
-        norm_max = np.max(sim.simulations[dynamics['Phosphorylated_cFos']])
-        error[6] = compute_objval_abs(
-            *diff_sim_and_exp(
-                sim.simulations[dynamics['Phosphorylated_cFos']],
-                exp.data[dynamics['Phosphorylated_cFos']],
-                exp_timepoint=exp.t2,num_condition=sim.condition,
-                norm_max_sim=norm_max
-            )
-        )
-
         return np.sum(error)
-
     else:
-
         return np.inf
