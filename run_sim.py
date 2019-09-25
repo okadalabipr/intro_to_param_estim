16e0ca40a731cfdd667ccc4977006e9b3fd9655f
himoto <himoto@protein.osaka-u.ac.jp>
2019-09-26 08:37:25 +0900

    Add description for args

--- run_sim.py
+++ run_sim.py
@@ -8,4 +8,9 @@ if not os.path.isdir('./figure'):
 from  param_estim.viz import visualize_result
 
 if __name__ == '__main__':
+    """=============================================================
+    viz_type: 'best', 'average', 'original' or int(1~n_fitparam)
+    show_all: bool
+    stdev: bool (Only when viz_type == 'average')
+    ================================================================"""
     visualize_result(viz_type='average',show_all=False,stdev=True)
