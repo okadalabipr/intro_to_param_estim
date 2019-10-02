45e586119bdf189ce0f8a65fda27737a1abfeeda
himoto <himoto@protein.osaka-u.ac.jp>
2019-10-02 11:23:41 +0900

    Update plot_func.py

--- param_estim/plot_func.py
+++ param_estim/plot_func.py
@@ -49,9 +49,10 @@ def timecourse(sim,n_file,viz_type,show_all,stdev,simulations_all):
                     sim.t, mean_hrg - yerr_hrg, mean_hrg + yerr_hrg,
                     lw=0,color='r',alpha=0.1
                 )
-
-        plt.plot(exp_t/60.,exp.data[j]['EGF'],'o',markerfacecolor='None',markeredgecolor='b',clip_on=False)
-        plt.plot(exp_t/60.,exp.data[j]['HRG'],'o',markerfacecolor='None',markeredgecolor='r',clip_on=False)
+                
+        if exp.data[j] is not None:
+            plt.plot(exp_t/60.,exp.data[j]['EGF'],'o',markerfacecolor='None',markeredgecolor='b',clip_on=False)
+            plt.plot(exp_t/60.,exp.data[j]['HRG'],'o',markerfacecolor='None',markeredgecolor='r',clip_on=False)
 
         plt.xlim(0,90)
         plt.xticks([0,30,60,90])
