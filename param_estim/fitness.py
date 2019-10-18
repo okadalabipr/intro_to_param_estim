133945ffe6ecef6a4d64d54863009d8ca23cac37
himoto <himoto@protein.osaka-u.ac.jp>
2019-10-18 10:42:28 +0900

    Add example

--- param_estim/fitness.py
+++ param_estim/fitness.py
@@ -66,6 +66,89 @@ def objective(individual_gene,search_idx,search_region):
                         norm_max_sim=norm_max
                     )
                 )
+        '''
+        error = np.zeros(16)
+        
+        norm_max = np.max(sim.simulations[dynamics['Phosphorylated_MEKc']])
+        error[0] = compute_objval_abs(
+            sim.simulations[dynamics['Phosphorylated_MEKc']][exp.t2,0]/norm_max,
+            exp.data[dynamics['Phosphorylated_MEKc']]['EGF']
+        )
+        error[1] = compute_objval_abs(
+            sim.simulations[dynamics['Phosphorylated_MEKc']][exp.t2,1]/norm_max,
+            exp.data[dynamics['Phosphorylated_MEKc']]['HRG']
+        )
+        
+        norm_max = np.max(sim.simulations[dynamics['Phosphorylated_ERKc']])
+        error[2] = compute_objval_abs(
+            sim.simulations[dynamics['Phosphorylated_ERKc']][exp.t2,0]/norm_max,
+            exp.data[dynamics['Phosphorylated_ERKc']]['EGF']
+        )
+        error[3] = compute_objval_abs(
+            sim.simulations[dynamics['Phosphorylated_MEKc']][exp.t2,1]/norm_max,
+            exp.data[dynamics['Phosphorylated_ERKc']]['HRG']
+        )
+        
+        norm_max = np.max(sim.simulations[dynamics['Phosphorylated_RSKw']])
+        error[4] = compute_objval_abs(
+            sim.simulations[dynamics['Phosphorylated_RSKw']][exp.t2,0]/norm_max,
+            exp.data[dynamics['Phosphorylated_RSKw']]['EGF']
+        )
+        error[5] = compute_objval_abs(
+            sim.simulations[dynamics['Phosphorylated_RSKw']][exp.t2,1]/norm_max,
+            exp.data[dynamics['Phosphorylated_RSKw']]['HRG']
+        )
+        
+        norm_max = np.max(sim.simulations[dynamics['Phosphorylated_CREBw']])
+        error[6] = compute_objval_abs(
+            sim.simulations[dynamics['Phosphorylated_CREBw']][exp.t3,0]/norm_max,
+            exp.data[dynamics['Phosphorylated_CREBw']]['EGF']
+        )
+        error[7] = compute_objval_abs(
+            sim.simulations[dynamics['Phosphorylated_CREBw']][exp.t3,1]/norm_max,
+            exp.data[dynamics['Phosphorylated_CREBw']]['HRG']
+        )
+        
+        norm_max = np.max(sim.simulations[dynamics['dusp_mRNA']])
+        error[8] = compute_objval_abs(
+            sim.simulations[dynamics['dusp_mRNA']][exp.t5,0]/norm_max,
+            exp.data[dynamics['dusp_mRNA']]['EGF']
+        )
+        error[9] = compute_objval_abs(
+            sim.simulations[dynamics['dusp_mRNA']][exp.t5,1]/norm_max,
+            exp.data[dynamics['dusp_mRNA']]['HRG']
+        )
+        
+        norm_max = np.max(sim.simulations[dynamics['cfos_mRNA']])
+        error[10] = compute_objval_abs(
+            sim.simulations[dynamics['cfos_mRNA']][exp.t4,0]/norm_max,
+            exp.data[dynamics['cfos_mRNA']]['EGF']
+        )
+        error[11] = compute_objval_abs(
+            sim.simulations[dynamics['cfos_mRNA']][exp.t4,1]/norm_max,
+            exp.data[dynamics['cfos_mRNA']]['HRG']
+        )
+        
+        norm_max = np.max(sim.simulations[dynamics['cFos_Protein']])
+        error[12] = compute_objval_abs(
+            sim.simulations[dynamics['cFos_Protein']][exp.t5,0]/norm_max,
+            exp.data[dynamics['cFos_Protein']]['EGF']
+        )
+        error[13] = compute_objval_abs(
+            sim.simulations[dynamics['cFos_Protein']][exp.t5,1]/norm_max,
+            exp.data[dynamics['cFos_Protein']]['HRG']
+        )
+        
+        norm_max = np.max(sim.simulations[dynamics['Phosphorylated_cFos']])
+        error[14] = compute_objval_abs(
+            sim.simulations[dynamics['Phosphorylated_cFos']][exp.t2,0]/norm_max,
+            exp.data[dynamics['Phosphorylated_cFos']]['EGF']
+        )
+        error[15] = compute_objval_abs(
+            sim.simulations[dynamics['Phosphorylated_cFos']][exp.t2,1]/norm_max,
+            exp.data[dynamics['Phosphorylated_cFos']]['HRG']
+        )
+        '''
         return np.sum(error)
     else:
         return np.inf
