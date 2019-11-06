12da919a191d5f2db512ce9ed15e4312d902e70a
himoto <himoto@protein.osaka-u.ac.jp>
2019-11-06 10:32:20 +0900

    Remove solveode

--- param_estim/observable.py
+++ param_estim/observable.py
@@ -33,30 +33,13 @@ def diff_sim_and_exp(
             list(exp_dict.values())
         ].flatten()/norm_max_exp
     )
-    
-
-def solveode(diffeq,y0,tspan,args):
-    sol = ode(diffeq)
-    sol.set_integrator('vode',method='bdf',min_step=1e-8,with_jacobian=True)
-    sol.set_initial_value(y0,tspan[0])
-    sol.set_f_params(args)
-
-    T = [tspan[0]]
-    Y = [y0]
-
-    while sol.successful() and sol.t < tspan[-1]:
-        sol.integrate(sol.t+1.)
-        T.append(sol.t)
-        Y.append(sol.y)
-
-    return np.array(T),np.array(Y)
 
 
 class NumericalSimulation(object):
     
     def _solveode(self,diffeq,y0,tspan,args):
         sol = ode(diffeq)
-        sol.set_integrator('vode',method='bdf',min_step=1e-8,with_jacobian=True)
+        sol.set_integrator('vode',method='bdf',with_jacobian=True,min_step=1e-8)
         sol.set_initial_value(y0,tspan[0])
         sol.set_f_params(args)
 
