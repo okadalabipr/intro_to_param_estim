e6cffc8be6fd1e980e9f3ededfef1051f690437f
himoto <himoto@protein.osaka-u.ac.jp>
2019-12-17 10:23:12 +0900

    Change pyplot rcParams

--- param_estim/plot_func.py
+++ param_estim/plot_func.py
@@ -9,17 +9,20 @@ def timecourse(sim,n_file,viz_type,show_all,stdev,simulations_all):
 
     exp = ExperimentalData()
     
-    plt.rcParams['font.size'] = 18
-    plt.rcParams['axes.linewidth'] = 1
+    plt.rcParams['font.size'] = 20
+    plt.rcParams['axes.linewidth'] = 1.2
+    plt.rcParams['xtick.major.width'] = 1.2
+    plt.rcParams['ytick.major.width'] = 1.2
     plt.rcParams['lines.linewidth'] = 2
     plt.rcParams['lines.markersize'] = 10
     plt.rcParams['font.family'] = 'Arial'
     plt.rcParams['mathtext.fontset'] = 'custom'
     plt.rcParams['mathtext.it'] = 'Arial:italic'
 
-    cmap = plt.get_cmap('tab10')
+    cmap = ['goldenrod','seagreen']
+    shape = ['^','o']
 
-    for i,title in enumerate(species):
+    for i,obs_name in enumerate(species):
 
         plt.figure(figsize=(4,3))
         plt.gca().spines['right'].set_visible(False)
@@ -30,24 +33,17 @@ def timecourse(sim,n_file,viz_type,show_all,stdev,simulations_all):
                 for l,_ in enumerate(sim.conditions):
                     plt.plot(
                         sim.t,simulations_all[i,j,:,l]/np.max(simulations_all[i,j,:,:]),
-                        color=cmap(l),alpha=0.05
+                        color=cmap[l],alpha=0.05
                     )
-
-        if not viz_type == 'average':
-            for l,_ in enumerate(sim.conditions):
-                plt.plot(
-                    sim.t,sim.simulations[i,:,l]/np.max(sim.simulations[i]),
-                    color=cmap(l)
-                )
-        else:
-            normalized = np.empty((len(species),n_file,len(sim.tspan),len(sim.conditions)))
+        if viz_type == 'average':
+            normalized = np.empty_like(simulations_all)
             for j in range(n_file):
                 for l,_ in enumerate(sim.conditions):
                     normalized[i,j,:,l] = simulations_all[i,j,:,l]/np.max(simulations_all[i,j,:,:])
             for l,_ in enumerate(sim.conditions):
                 plt.plot(
                     sim.t,np.nanmean(normalized[i,:,:,l],axis=0),
-                    color=cmap(l)
+                    color=cmap[l]
                 )
             if stdev:
                 for l,_ in enumerate(sim.conditions):
@@ -55,7 +51,21 @@ def timecourse(sim,n_file,viz_type,show_all,stdev,simulations_all):
                     yerr = [np.nanstd(normalized[i,:,k,l],ddof=1) for k,_ in enumerate(sim.t)]
                     plt.fill_between(
                         sim.t, mean - yerr, mean + yerr,
-                        lw=0,color=cmap(l),alpha=0.1
+                        lw=0,color=cmap[l],alpha=0.1
+                    )
+        else:
+            for l,_ in enumerate(sim.conditions):
+                plt.plot(
+                    sim.t,sim.simulations[i,:,l]/np.max(sim.simulations[i]),
+                    color=cmap[l]
+                )
+        if exp.experiments[i] is not None:
+            exp_t = exp.get_timepoint(i)
+            for l,condition in enumerate(sim.conditions):
+                if condition in exp.experiments[i]:
+                    plt.plot(
+                        np.array(exp_t)/60.,exp.experiments[i][condition],shape[l],
+                        markerfacecolor='None',markeredgecolor=cmap[l],clip_on=False
                     )
 
         if exp.experiments[i] is not None:
@@ -63,10 +73,8 @@ def timecourse(sim,n_file,viz_type,show_all,stdev,simulations_all):
             for l,condition in enumerate(sim.conditions):
                 if condition in exp.experiments[i]:
                     plt.plot(
-                        np.array(exp_t)/60.,exp.experiments[i][condition],'D',
-                        markerfacecolor='None',
-                        markeredgecolor=cmap(l),
-                        clip_on=False
+                        np.array(exp_t)/60.,exp.experiments[i][condition],shape[l],
+                        markerfacecolor='None',markeredgecolor=cmap[l],clip_on=False
                     )
 
         plt.xlim(0,90)
@@ -83,6 +91,15 @@ def timecourse(sim,n_file,viz_type,show_all,stdev,simulations_all):
         
         
 def param_range(search_idx,search_param_matrix,portrait):
+    
+    plt.rcParams['font.size'] = 12
+    plt.rcParams['axes.linewidth'] = 1.2
+    plt.rcParams['xtick.major.width'] = 1.2
+    plt.rcParams['ytick.major.width'] = 1.2
+    plt.rcParams['font.family'] = 'Arial'
+    plt.rcParams['mathtext.fontset'] = 'custom'
+    plt.rcParams['mathtext.it'] = 'Arial:italic'
+    
     if portrait:
         if len(search_idx[0]) > 0:
             fig = plt.figure(figsize=(8,len(search_idx[0])/2.5))
@@ -102,7 +119,9 @@ def param_range(search_idx,search_param_matrix,portrait):
             ax.set_yticklabels([model.C.param_names[i] for i in search_idx[0]])
             ax.set_xscale('log')
 
-            plt.savefig('./figure/param_range.pdf',bbox_inches='tight')
+            plt.savefig(
+                './figure/param_range.pdf',bbox_inches='tight'
+            )
             plt.close(fig)
         if len(search_idx[1]) > 0:
             fig = plt.figure(figsize=(8,len(search_idx[1])/2.5))
@@ -122,7 +141,9 @@ def param_range(search_idx,search_param_matrix,portrait):
             ax.set_yticklabels([model.V.var_names[i] for i in search_idx[1]])
             ax.set_xscale('log')
 
-            plt.savefig('./figure/initial_value_range.pdf',bbox_inches='tight')
+            plt.savefig(
+                './figure/initial_value_range.pdf',bbox_inches='tight'
+            )
             plt.close(fig)
     else:
         if len(search_idx[0]) > 0:
@@ -142,7 +163,9 @@ def param_range(search_idx,search_param_matrix,portrait):
             ax.set_ylabel('Parameter value')
             ax.set_yscale('log')
 
-            plt.savefig('./figure/param_range_h.pdf',bbox_inches='tight')
+            plt.savefig(
+                './figure/param_range_h.pdf',bbox_inches='tight'
+            )
             plt.close(fig)
         if len(search_idx[1]) > 0:
             fig = plt.figure(figsize=(len(search_idx[1])/2.2,6))
@@ -161,5 +184,7 @@ def param_range(search_idx,search_param_matrix,portrait):
             ax.set_ylabel('Initial value')
             ax.set_yscale('log')
 
-            plt.savefig('./figure/initail_value_range_h.pdf',bbox_inches='tight')
+            plt.savefig(
+                './figure/initail_value_range_h.pdf',bbox_inches='tight'
+            )
             plt.close(fig)
