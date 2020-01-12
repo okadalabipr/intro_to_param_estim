1993215172e5dfb7823eb1a9b2985c00863ce191
himoto <himoto@protein.osaka-u.ac.jp>
2020-01-12 16:47:38 +0900

    code cleanup

--- optimize_continue.py
+++ optimize_continue.py
@@ -1,3 +1,4 @@
+from param_estim import optimize, optimize_continue
 import os
 import sys
 import re
@@ -5,19 +6,19 @@ import warnings
 import multiprocessing
 warnings.filterwarnings('ignore')
 
-from param_estim import optimize,optimize_continue
 
 def run_ga_continue(nth_paramset):
-    if not os.path.isdir('./out/%d'%(nth_paramset)):
-        os.mkdir('./out/%d'%(nth_paramset))
+    if not os.path.isdir('./out/%d' % (nth_paramset)):
+        os.mkdir('./out/%d' % (nth_paramset))
         optimize(nth_paramset)
     else:
         optimize_continue(nth_paramset)
-        
+
+
 if __name__ == '__main__':
     args = sys.argv
     if 'current_ipynb' in globals():
-        nth_paramset = int(re.sub(r'\D','',current_ipynb))
+        nth_paramset = int(re.sub(r'\D', '', current_ipynb))
         run_ga_continue(nth_paramset)
     else:
         if len(args) == 2:
@@ -25,5 +26,5 @@ if __name__ == '__main__':
         elif len(args) == 3:
             n_proc = max(1, multiprocessing.cpu_count() - 1)
             p = multiprocessing.Pool(processes=n_proc)
-            p.map(run_ga_continue, range(int(args[1]),int(args[2])+1))
+            p.map(run_ga_continue, range(int(args[1]), int(args[2])+1))
             p.close()
