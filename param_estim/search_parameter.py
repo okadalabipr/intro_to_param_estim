a09335b248ff7586bbf834745768cfd08ef635c3
himoto <himoto@protein.osaka-u.ac.jp>
2020-03-14 14:53:00 +0900

    Update search_parameter.py

--- param_estim/search_parameter.py
+++ param_estim/search_parameter.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import numpy as np
 
 from model.name2idx import parameters as C
@@ -10,7 +8,7 @@ from model.initial_condition import initial_values
 def search_parameter_index():
 
     # Write param index for optimization
-    search_idx_const = np.array([
+    search_idx_const = [
         C.V1,
         C.Km1,
         C.V5,
@@ -86,12 +84,12 @@ def search_parameter_index():
         C.KF31,
         C.nF31,
         C.a,
-    ])
+    ]
 
     # initialvalues
-    search_idx_init = np.array([
+    search_idx_init = [
         # V.(variable name)
-    ])
+    ]
 
     return search_idx_const, search_idx_init
 
@@ -103,11 +101,9 @@ def get_search_region():
     search_idx = search_parameter_index()
 
     if len(search_idx[0]) != len(set(search_idx[0])):
-        print('Error: Duplicate param name.')
-        sys.exit()
+        raise ValueError('Duplicate param name.')
     elif len(search_idx[1]) != len(set(search_idx[1])):
-        print('Error: Duplicate var name.')
-        sys.exit()
+        raise ValueError('Duplicate var name.')
     else:
         pass
 
@@ -122,32 +118,31 @@ def get_search_region():
         for _, idx in enumerate(search_idx[0]):
             if x[int(idx)] == 0.:
                 raise ValueError(
-                    '"C.%s" in search_idx_const: ' % (
+                    '"C.{}" in search_idx_const: '.format(
                         C.param_names[int(idx)]
                     ) + message
                 )
         for _, idx in enumerate(search_idx[1]):
             if y0[int(idx)] == 0.:
                 raise ValueError(
-                    '"V.%s" in search_idx_init: ' % (
+                    '"V.{}" in search_idx_init: '.format(
                         V.var_names[int(idx)]
                     ) + message
                 )
-        sys.exit()
 
     search_region = np.zeros((2, len(x)+len(y0)))
 
     # Default: 0.1 ~ 10
     for i, j in enumerate(search_idx[0]):
-        search_region[0, j] = search_param[i]*0.1  # lower bound
-        search_region[1, j] = search_param[i]*10.  # upper bound
+        search_region[0, j] = search_param[i] * 0.1  # lower bound
+        search_region[1, j] = search_param[i] * 10.  # upper bound
 
     # Default: 0.5 ~ 2
     for i, j in enumerate(search_idx[1]):
         search_region[0, j+len(x)] = \
-            search_param[i+len(search_idx[0])]*0.5  # lower bound
+            search_param[i+len(search_idx[0])] * 0.5  # lower bound
         search_region[1, j+len(x)] = \
-            search_param[i+len(search_idx[0])]*2.0  # upper bound
+            search_param[i+len(search_idx[0])] * 2.0  # upper bound
 
     # search_region[:,C.param_name] = [lower_bound,upper_bound]
     # search_region[:,V.var_name+len(x)] = [lower_bound,upper_bound]
@@ -252,27 +247,27 @@ def lin2log(search_idx, search_region, n_param_const, n_search_param):
             message = 'search_region[lb,ub] must be positive.'
             if i <= n_param_const:
                 raise ValueError(
-                    '"C.%s": ' % (
+                    '"C.{}": '.format(
                         C.param_names[i]
                     ) + message
                 )
             else:
                 raise ValueError(
-                    '"V.%s": ' % (
+                    '"V.{}": '.format(
                         V.var_names[i-n_param_const]
                     ) + message
                 )
-        elif np.min(search_region[:, i]) == 0.0 and np.max(search_region[:, i]) != 0:
+        elif np.min(search_region[:, i]) == 0 and np.max(search_region[:, i]) != 0:
             message = 'lower_bound must be larger than 0.'
             if i <= n_param_const:
                 raise ValueError(
-                    '"C.%s" ' % (
+                    '"C.{}" '.format(
                         C.param_names[i]
                     ) + message
                 )
             else:
                 raise ValueError(
-                    '"V.%s" ' % (
+                    '"V.{}" '.format(
                         V.var_names[i-n_param_const]
                     ) + message
                 )
@@ -280,37 +275,43 @@ def lin2log(search_idx, search_region, n_param_const, n_search_param):
             message = 'lower_bound < upper_bound'
             if i <= n_param_const:
                 raise ValueError(
-                    '"C.%s" : ' % (
+                    '"C.{}" : '.format(
                         C.param_names[i]
                     ) + message
                 )
             else:
                 raise ValueError(
-                    '"V.%s" : ' % (
+                    '"V.{}" : '.format(
                         V.var_names[i-n_param_const]
                     ) + message
                 )
     difference = list(
-        set(np.where(np.any(search_region != 0., axis=0))[0]) ^
-        set(np.append(search_idx[0], n_param_const+search_idx[1]))
+        set(
+            np.where(
+                np.any(search_region != 0., axis=0)
+            )[0]
+        ) ^ set(
+            np.append(
+                search_idx[0],
+                [n_param_const + idx for (_, idx) in enumerate(search_idx[1])]
+            )
+        )
     )
     if len(difference) > 0:
         message = 'in both search_idx and search_region'
         for i, j in enumerate(difference):
             if j <= n_param_const:
-                print(
-                    'Set "C.%s" ' % (
+                raise ValueError(
+                    'Set "C.{}" '.format(
                         C.param_names[int(j)]
                     ) + message
                 )
             else:
-                print(
-                    'Set "V.%s" ' % (
+                raise ValueError(
+                    'Set "V.{}" '.format(
                         V.var_names[int(j-n_param_const)]
                     ) + message
                 )
-        sys.exit()
-
     search_region = search_region[:, np.any(search_region != 0., axis=0)]
 
     return np.log10(search_region)
